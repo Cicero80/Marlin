46cfc339673eeae485c32015aa84f0b19a0b4b36
Cicero80 <41273351+Cicero80@users.noreply.github.com>
2020-06-17 11:38:16 -0500

    June17-noscurve

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -828,7 +828,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+//#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
@@ -980,7 +980,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -23, 16, -3.00 }
+#define NOZZLE_TO_PROBE_OFFSET { -23, 16, -2.99 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
